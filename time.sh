b3b71b0f5ee76bf9749ff672be98fe0899a83c1b
Eeo Jun <141bytes@gmail.com>
2020-01-02 23:35:56 +0000

    update

--- time.sh
+++ time.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-lines="200
-300
-500
-700
-900"
+lines="6
+7
+8
+9
+10
+11
+12
+13"
 
 for i in $lines; do
     echo "$i"
     for j in $(seq 5); do
-        { time ./solution-step4 $(cat "$i/initial-conditions-no-plot.txt"); } 2> "time-2-$i-$j.txt"
+        { time ./solution-step4 $(cat "$i/initial-conditions-no-plot.txt"); } 2> "timing/time-$i-$j.txt"
     done
 done
